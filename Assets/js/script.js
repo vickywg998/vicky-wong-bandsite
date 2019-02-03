eeafccc2909092e1efba9ba68c13bb07b0b60b47
Vicky Wong <vickywg998@hotmail.com>
2019-02-03 14:29:36 -0800

    updated comment section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,105 +1,97 @@
 var userComments = {
-  sampleComment1: [{
+  sampleComment1: {
     "name": 'Micheal Lyons',
    "date":'02/01/2019',
-    "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."}],
-  sampleComment2:[{
+    "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."},
+  sampleComment2:{
     "name": 'Gary Wong',
     "date":'01/31/2018',
-    "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"}],
-  sampleComment3: [{
+    "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"},
+  sampleComment3: {
     "name": 'Theodore Duncan',
     "date":'01/24/2018',
-    "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"}]
+    "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"}
 }
 
 
+// Excuse my thought process here! LOL
+//activate button by selecting ID
 var clickMe = document.getElementById("comment_button");
-clickMe.addEventListener("click", appendComments);
+// Created a comment event handler to handle the click in the form submission
+clickMe.addEventListener("click", commentEventHandler);
 
-function createComment(comment, value) {
-  const element = document.createElement('div')
-  element.innerText = value
-  return element
+function createComment(name, comment) {
+  const element = document.createElement('div');
+  // styled the comments. The class is static_comment
+  element.className = "static_comment";
+  // It should have an icon, and a content div
+  const icon = document.createElement('div');
+  icon.className = "singer_icon";
+  const icon_pic = document.createElement('img');
+icon_pic.src ="Assets/Images/doge.jpg";
+  icon_pic.className = "icons";
+  // styled the comment content
+  const content = document.createElement('div');
+  content.className = "comment_content";
+
+  // Each comment content div also has two things within it
+  // a comment_name of type h4 and a para of type p
+  // there's also a date, which is not handled right now
+  // TODO: Handle the date
+  const comment_name = document.createElement('h4');
+  comment_name.className = "comment_name";
+  comment_name.innerHTML = name;
+  //added icon! 
+
+  // Let's just create a static date and handle it later.
+  const date = document.createElement('span');
+  date.className = "comment_date";
+  date.innerHTML = "02/02/2019";
+  // Now we add the paragraph
+  const paragraph = document.createElement('p');
+  paragraph.className = "para";
+  paragraph.innerHTML = comment;
+
+  // Add the paragraph and the comment name to the content div
+  content.appendChild(comment_name);
+  content.appendChild(date);
+  content.appendChild(paragraph);
+
+  // OK! At this point the comment is complete (except the date)
+
+  // Lets add everything to the parent div
+  element.appendChild(icon);
+  element.appendChild(content);
+//push icon to div 
+
+
+
+icon.appendChild(icon_pic);
+// add icon to real time comment
+
+  return element;
+  return icon;
 }
 
-function appendComments (event) {
-  event.preventDefault()
-  const name = document.querySelector('#name').value
-  const comment = document.querySelector('#comment').value
-  const newComments = document.querySelector('#newComment')
+function appendComments(name, comment) {
+  const newComments = document.querySelector('#comments');
   
-  newComments.appendChild(createComment('name', name))
-  newComments.appendChild(createComment('comment', comment))
+  newComments.appendChild(createComment(name, comment));
 }
-const commentList = document.querySelector('#comment_list p');
 
-//create elements
-const new_comment_div = document.createElement("div");
-const iconPush = document.createElement('div');
-const comment_contentPush = document.createElement('div');
-const comment_namePush = document.createElement('h4');
-const comment_datePush = document.createElement('span');
-const paragraphPush = document.createElement('p');
-
-
-//append to document 
-comment_contentPush.appendChild(comment_namePush);
-comment_contentPush.appendChild(comment_datePush);
-comment_contentPush.appendChild(paragraphPush);
-commentList.appendChild(comment_contentPush);
-
-// push class name 
-new_comment_div.className += "static_comment";
-iconPush.className += "singer_icon";
-comment_contentPush.className += "comment_content";
-comment_namePush.className += "comment_name";
-comment_datePush.className += "comment_date";
-paragraphPush.className += "para";
-
-
-// add content 
-paragraphPush.textContent = value;
-
-
-/*
-user_comment.sample_comment1['name']
-const comment_wrapper = document.getElementById('comments');
-const new_comment_div = document.createElement("div");
-var node = document.createTextNode("I really like this artist. He's great");
-
-new_comment_div.className += "static_comment";
-
-const iconPush = document.createElement('div');
-iconPush.className += "singer_icon";
-
-const comment_contentPush = document.createElement('div');
-comment_contentPush.className += "comment_content";
-
-const comment_namePush = document.createElement('h4');
-comment_namePush.className += "comment_name";
-
-const comment_datePush = document.createElement('span');
-comment_datePush.className += "comment_date";
-
-const paragraphPush = document.createElement('p');
-paragraphPush.className += "para";
-
-comment_wrapper.appendChild(new_comment_div);
-new_comment_div.appendChild(iconPush);
-paragraphPush.appendChild(node);
-new_comment_div.appendChild(comment_contentPush);
-
-comment_contentPush.appendChild(comment_namePush);
-comment_contentPush.appendChild(comment_datePush);
-
-comment_contentPush.appendChild(paragraphPush);
-
-function displayComment() {
-
-  var new_comment = document.createComment("I really like this artist. He's great");
-  paragraphPush.appendChild(new_comment);
+// Here I separated the event handler from the function that
+// makes the actual comments (appendComments)
+function commentEventHandler(event) {
+  event.preventDefault();
+  const name = document.querySelector('#name').value;
+  const comment = document.querySelector('#comment').value;
   
+  appendComments(name, comment);
 }
 
-*/
+for (var commentName in userComments) {
+	commentObj = userComments[commentName];
+	console.log(commentObj);
+	
+	appendComments(commentObj.name, commentObj.comment);
+}
