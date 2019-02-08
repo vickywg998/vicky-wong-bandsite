9a50cfa777a56884069765e68e7301648b50b0c9
Vicky Wong <vickywg998@hotmail.com>
2019-02-07 16:12:10 -0800

    get request using axios

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,91 +1,188 @@
-var userComments = {
-  sampleComment1: {
-    "name": 'Micheal Lyons',
-   "date":'02/01/2019',
-    "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."},
-  sampleComment2:{
-    "name": 'Gary Wong',
-    "date":'01/31/2018',
-    "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"},
-  sampleComment3: {
-    "name": 'Theodore Duncan',
-    "date":'01/24/2018',
-    "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"}
+const myAPIKey = '62d66fd2-00ae-4ece-aae4-e49bd1f8eccd';
+const commentURL = 'https://project-1-api.herokuapp.com/comments?api_key=${myAPIKey}';
+const showURL = 'https://project-1-api.herokuapp.com/showdates?api_key=${myAPIKey}'
+
+//get request using axios 
+window.onload= function() {
+axios.get(commentURL)
+.then(function(response) {
+  const users = response.data;
+console.log(users);
+console.log(response)
+
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+
+
+const ul= document.querySelector('#comments');
+users.map(user => {
+const newComments = document.createElement('div');
+newComments.innerHTML = '<div class="static_comment">' + '</div>'+ '<div class="singer_icon">'+ '<img class="icons">' + '</img>'   + '</div>' +'<div class="comment_content">' + '<h4 class="comment_name">' + user.name + '</h4>' + '<span class="comment_date">' + user.timestamp + '</span>' + '<p class="para">' + user.comment + '</p>'+ '</div>' 
+
+
+return newComments;
+}).forEach (newComments => {
+  ul.appendChild(newComments)
+})
+ 
 }
+)}
 
 
-// Excuse my thought process here! LOL
-//activate button by selecting ID
-var clickMe = document.getElementById("comment_button");
-// Created a comment event handler to handle the click in the form submission
-clickMe.addEventListener("click", commentEventHandler);
-
-function createComment(name, comment) {
-  const element = document.createElement('div');
-  // styled the comments. The class is static_comment
-  element.className = "static_comment";
-  // created div and img class for icons, added static icon.
-  const icon = document.createElement('div');
-  icon.className = "singer_icon";
-  const icon_pic = document.createElement('img');
-icon_pic.src ="Assets/Images/doge.jpg";
-  icon_pic.className = "icons";
-  // styled the comment content
-  const content = document.createElement('div');
-  content.className = "comment_content";
-
-  // Each comment content div also has two things within it
-  // a comment_name of type h4 and a para of type p
-  const comment_name = document.createElement('h4');
-  comment_name.className = "comment_name";
-  comment_name.innerHTML = name;
 
 
-  //created a static date.
-  const date = document.createElement('span');
-  date.className = "comment_date";
-  date.innerHTML = "02/02/2019";
-  // added the paragraph
-  const paragraph = document.createElement('p');
-  paragraph.className = "para";
-  paragraph.innerHTML = comment;
+//post request axios 
 
-  // Added the paragraph and the comment name to the content div
-  content.appendChild(comment_name);
-  content.appendChild(date);
-  content.appendChild(paragraph);
 
-  // the comment is complete (except the date)
-
-  // add everything to the parent div
-  element.appendChild(icon);
-  element.appendChild(content);
-//push icon to div 
-icon.appendChild(icon_pic);
-// add icon to real time comment
-  return element;
-  return icon;
-}
-
-function appendComments(name, comment) {
-  const newComments = document.querySelector('#comments');
+// window.onload= function() {
+//   axios.get(commentURL)
+//   .then(function(response) {
+//     const users = response.data;
+//   console.log(users);
   
-  newComments.appendChild(createComment(name, comment));
-}
-
-// separated the event handler from the function that
-// makes the actual comments (appendComments)
-function commentEventHandler(event) {
-  event.preventDefault();
-  const name = document.querySelector('#name').value;
-  const comment = document.querySelector('#comment').value;
+//   const ul= document.querySelector('#comments');
+//   users.map(user => {
+//   const newUserComments = document.createElement('div');
+//   newUserComments.innerHTML = response.data;
+//   return newUserComments;
+//   }).forEach(div => {
+//     const newUserComments = document.createElement('div');
+//   newUserComments.innerHTML = response.data[0].name;
+//     ul.appendChild(newUserComments);
+//   })
   
-  appendComments(name, comment);
-}
+//   })
+//   }
+// const user = {
+//   userId: 11,
+//   title:'My Article',
+//   body: 'this is the main article content'
+// }
+// axios.post(postsUrl, user)
+// .then(response => {
+//   console.log(response)
+// })
 
-for (var commentName in userComments) {
-	commentObj = userComments[commentName];
-	console.log(commentObj);
+// const userId = 1
+// axios.get(`postsUrl?userId=${userId}`)
+// .then(response => {
+//   console.log(response.data)
+// })
+// const arrData = response.data;
+// const nameData = response.data[0].name
+// console.log(nameData)
+// console.log(arrData);
+
+
+// $('.comment_icon').click((event) => {
+  // const nameData = response.data[0].name
+//   event.preventDefault()
+//   axios
+//     .get(commentURL)
+//     .then(response => {
+//       const nameData = response.name;
+//       const commentData = response.comment;
+//       const dateData = response.timestamp;
+//       $('.static_comments').append(`
+//         <h4 class "comment_name">${response.name}</h4>
+//       `)      
+//     })
+//     .catch(error => {
+//       console.log(error)
+//     })
+// });
+
+
+
+
+// var userComments = {
+//   sampleComment1: {
+//     "name": 'Micheal Lyons',
+//    "date":'02/01/2019',
+//     "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."},
+//   sampleComment2:{
+//     "name": 'Gary Wong',
+//     "date":'01/31/2018',
+//     "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"},
+//   sampleComment3: {
+//     "name": 'Theodore Duncan',
+//     "date":'01/24/2018',
+//     "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"}
+// }
+
+
+// // Excuse my thought process here! LOL
+// //activate button by selecting ID
+// var clickMe = document.getElementById("comment_button");
+// // Created a comment event handler to handle the click in the form submission
+// clickMe.addEventListener("click", commentEventHandler);
+
+// function createComment(name, comment) {
+//   const element = document.createElement('div');
+//   // styled the comments. The class is static_comment
+//   element.className = "static_comment";
+//   // created div and img class for icons, added static icon.
+//   const icon = document.createElement('div');
+//   icon.className = "singer_icon";
+//   const icon_pic = document.createElement('img');
+// icon_pic.src ="Assets/Images/doge.jpg";
+//   icon_pic.className = "icons";
+//   // styled the comment content
+//   const content = document.createElement('div');
+//   content.className = "comment_content";
+
+//   // Each comment content div also has two things within it
+//   // a comment_name of type h4 and a para of type p
+//   const comment_name = document.createElement('h4');
+//   comment_name.className = "comment_name";
+//   comment_name.innerHTML = name;
+
+
+//   //created a static date.
+//   const date = document.createElement('span');
+//   date.className = "comment_date";
+//   date.innerHTML = "02/02/2019";
+//   // added the paragraph
+//   const paragraph = document.createElement('p');
+//   paragraph.className = "para";
+//   paragraph.innerHTML = comment;
+
+//   // Added the paragraph and the comment name to the content div
+//   content.appendChild(comment_name);
+//   content.appendChild(date);
+//   content.appendChild(paragraph);
+
+//   // the comment is complete (except the date)
+
+//   // add everything to the parent div
+//   element.appendChild(icon);
+//   element.appendChild(content);
+// //push icon to div 
+// icon.appendChild(icon_pic);
+// // add icon to real time comment
+//   return element;
+//   return icon;
+// }
+
+// function appendComments(name, comment) {
+//   const newComments = document.querySelector('#comments');
+  
+//   newComments.appendChild(createComment(name, comment));
+// }
+
+// // separated the event handler from the function that
+// // makes the actual comments (appendComments)
+// function commentEventHandler(event) {
+//   event.preventDefault();
+//   const name = document.querySelector('#name').value;
+//   const comment = document.querySelector('#comment').value;
+  
+//   appendComments(name, comment);
+// }
+
+// for (var commentName in userComments) {
+// 	commentObj = userComments[commentName];
+// 	console.log(commentObj);
 	
-	appendComments(commentObj.name, commentObj.comment);
-}
+// 	appendComments(commentObj.name, commentObj.comment);
+// 
